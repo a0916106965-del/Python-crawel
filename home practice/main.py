da62b048db59c11124a012ddf9544b969faae42a
show <showching_01@yahoo.com.tw>
2026-01-10 16:23:47 +0800

    20260110變更

---
+++ home practice/main.py
@@ -0,0 +1,9 @@
+#主程式
+import geometry.point
+result=geometry.point.distance(3,4)
+print("點(3,4)到原點的距離為:",result)
+import geometry.line
+result2=geometry.line.slope(1,2,3,4)
+print("直線(1,2)到(3,4)的斜率為:",result2)
+
+
